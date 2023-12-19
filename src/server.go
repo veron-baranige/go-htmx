aaf3fa0fbfddfc1a3196134a5a45b2620ae626a5
Veron Baranige <veron.b@hsenidoutsourcing,com>
2023-12-20 00:58:33 +0530

    Set up routes and server static files

--- src/server.go
+++ src/server.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/veron-baranige/todo-htmx/src/routes"
 )
 
 func main() {
     e := echo.New()
+    e.Static("/static", "static")
+    routes.Setup(e)
     e.Logger.Fatal(e.Start(":8080"))
 }
